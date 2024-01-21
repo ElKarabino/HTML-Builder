e171d7f1d46002e71634aa69c129ca9b6bd0b250
Stanislau Karbanovich <karbanovichstanislav@gmail.com>
2024-01-21 21:44:23 +0100

    feat: add 2nd task

---
+++ 02-write-file/index.js
@@ -0,0 +1,23 @@
+const fs = require('node:fs');
+const readline = require('readline');
+const filePath = '02-write-file.txt';
+const startText = 'Write some text: ';
+const writeStream = fs.createWriteStream(filePath, { flags: 'a', encoding: 'utf-8' });
+const rl = readline.createInterface(process.stdin, process.stdout);
+function input() {
+  rl.question(startText, (text) => {
+    if (text === 'exit') {
+      console.log('GoodBye!');
+      rl.close();
+    }
+    else {
+      writeStream.write(text + '\n');
+      input();
+    }
+  });
+  rl.on('SIGINT', () => {
+    console.log('\nGoodbye!');
+    rl.close();
+  });
+};
+input();

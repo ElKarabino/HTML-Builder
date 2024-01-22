2bef2fd62d8bd6a987ae67d221c802626696d594
Stanislau Karbanovich <karbanovichstanislav@gmail.com>
2024-01-22 20:43:25 +0100

    feat: add 4th task

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,27 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const folderPath = '04-copy-directory';
+
+fs.mkdir(path.join(folderPath, 'files-copy'),
+  { recursive: true },
+  (err) => {
+    if (err) {
+      console.error('err');
+    }
+    console.log('Directory created successfully!');
+  });
+fs.readdir(path.join(folderPath, 'files'), (err, files) => {
+  if (err) {
+    console.log('err');
+  } else {
+    files.forEach((file) => {
+      const sourcePath = path.join(path.join(folderPath, 'files'),file);
+      const destinationPath = path.join(path.join(folderPath, 'files-copy'),file);
+      fs.copyFile(sourcePath, destinationPath, (err) => {
+        if (err) {
+          console.log('Error');
+        } 
+      });
+    });
+  }
+});
